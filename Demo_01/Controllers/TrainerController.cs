fa3fbe74aa40b1aacf4295c72a229a7486c4e998
Antoine Tilman <antoine.tilman@gmail.com>
2023-10-02 10:06:23 +0200

    Correction AUDE

--- Demo_01/Controllers/TrainerController.cs
+++ Demo_01/Controllers/TrainerController.cs
@@ -1,7 +1,10 @@
 ﻿using Demo_01.DAL.Entities;
 using Demo_01.DAL.Repositories;
+using Demo_01.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
+using System.Net;
 
 namespace Demo_01.Controllers
 {
@@ -10,26 +13,29 @@ namespace Demo_01.Controllers
     public class TrainerController : ControllerBase
     {
         private TrainerRepository _TrainerRepository;
-
         public TrainerController(TrainerRepository trainerRepository)
         {
             _TrainerRepository = trainerRepository;
-            //// 1 (mauvaise) facon d'injecter notre TrainerRepositoryà
+            // 1 (mauvaise) façon d'injecter notre TrainerRepository
             //_TrainerRepository = new TrainerRepository();
         }
 
-        [HttpGet] //Permet de faire comprendre à swagger que la méthode ci dessous est une méthode get
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Trainer>))]
         public IActionResult GetTrainers()
         {
             IEnumerable<Trainer> trainers = _TrainerRepository.GetAll();
             return Ok(trainers);
-            //Demo avant qu'on mette la liste etou
+
+            // Demo avant qu'on mette la liste étou
             //return Ok(new { message = "Hello les .Net de Technobel" });
             //OU
-            //retrun StatusCode(StatusCodes.Status200OK, "Hello les .Net de Technobel");
+            //return StatusCode(StatusCodes.Status200OK, "Hello les .Net de Technobel");
         }
 
-        [HttpGet("{trainerId}")] 
+        [HttpGet("{trainerId:int:min(1)}")]
+        [ProducesResponseType(200, Type = typeof(Trainer))]
+        [ProducesResponseType(404)]
         public IActionResult GetTrainer(int trainerId)
         {
             Trainer? trainer = _TrainerRepository.GetById(trainerId);
@@ -38,6 +44,50 @@ namespace Demo_01.Controllers
                 return NotFound();
             }
             return Ok(trainer);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Trainer))]
+        public IActionResult AddTrainer(TrainerDTO trainer)
+        {
+            Trainer trainerToAdd = new Trainer
+            {
+                LastName = trainer.LastName,
+                FirstName = trainer.FirstName,
+                BirthDate = trainer.BirthDate
+            };
+
+            Trainer trainerAdded = _TrainerRepository.Create(trainerToAdd);
+
+            // Renvoie d'une 201
+            return CreatedAtAction
+                (
+                    nameof(GetTrainer), //string : nom de l'action à mettre dans le location
+                    new { trainerId = trainerAdded.Id }, //objet : contenant tous les paramètres dont la route a besoin (ici notre GetTrainer a besoin de trainerId)
+                    trainerAdded //objet : représente l'objet qui vient d'être crée (qui sera dans le body de la reponse)
+                );
+        }
+
+        [HttpDelete("{trainerId:int}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteTrainer(int trainerId)
+        {
+            if (_TrainerRepository.Delete(trainerId))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{trainerId:int}")]
+        [ProducesResponseType(501)]
+        public IActionResult UpdateTrainer(int trainerId, TrainerDTO trainer)
+        {
+            return StatusCode(StatusCodes.Status501NotImplemented, new { reason = "Flemme" });
         }
     }
 }
