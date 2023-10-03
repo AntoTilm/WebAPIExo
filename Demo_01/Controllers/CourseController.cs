93b9e31d374eeb12d655717cb75eba06fcf7528d
Antoine Tilman <antoine.tilman@gmail.com>
2023-10-03 11:00:56 +0200

    Création DB + DAL en cours

--- Demo_01/Controllers/CourseController.cs
+++ Demo_01/Controllers/CourseController.cs
@@ -31,8 +31,8 @@ namespace Demo_01.Controllers
         }
 
         [HttpGet("{courseId:int:min(1)}")]
-        //[ProducesResponseType(200, Type = typeof(Course))]
-        //[ProducesResponseType(404)]
+        [ProducesResponseType(200, Type = typeof(Course))]
+        [ProducesResponseType(404)]
         public IActionResult GetCourse(int courseId)
         {
             Course? course = _CourseRepository.GetById(courseId);
@@ -43,48 +43,53 @@ namespace Demo_01.Controllers
             return Ok(course);
         }
 
-        //[HttpPost]
-        //[ProducesResponseType(201, Type = typeof(Course))]
-        //public IActionResult AddCourse(CourseDTO course)
-        //{
-            //Course courseToAdd = new Course
-            //{
-            //    Id = course.Id,
-            //    Name = course.Name,
-            //    Description = course.Description
-            //};
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Course))]
+        public IActionResult AddCourse(CourseDTO course)
+        {
+            Course courseToAdd = new Course
+            {
+                Name = course.Name,
+                Description = course.Description
+            };
+            // L'objet créé ci dessus permet de fournir courseToAdd au _CourseRepository qui est de type course.cs et non course qui est de type CourseDTO
+            Course courseAdded = _CourseRepository.Create(courseToAdd);
 
-            //Course courseAdded = _CourseRepository.Create(courseToAdd);
+            //Renvoie d'une 201
+            return CreatedAtAction
+                (
+                    nameof(GetById), //string : nom de l'action à mettre dans le location
+                    new { courseId = CourseToAdd.Id }, //objet : contenant tous les paramètres dont la route a besoin (ici notre GetTrainer a besoin de trainerId)
+                    courseAdded //objet : représente l'objet qui vient d'être crée (qui sera dans le body de la reponse)
+                );
+        }
 
-            // Renvoie d'une 201
-            //return CreatedAtAction
-            //    (
-            //        nameof(GetTrainer), //string : nom de l'action à mettre dans le location
-            //        new { trainerId = trainerAdded.Id }, //objet : contenant tous les paramètres dont la route a besoin (ici notre GetTrainer a besoin de trainerId)
-            //        trainerAdded //objet : représente l'objet qui vient d'être crée (qui sera dans le body de la reponse)
-            //    );
-        //}
+        [HttpDelete("{trainerId:int}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult DeleteTrainer(int courseId)
+        {
+            if (_CourseRepository.Delete(courseId))
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
 
-        //[HttpDelete("{trainerId:int}")]
-        //[ProducesResponseType(204)]
-        //[ProducesResponseType(404)]
-        //public IActionResult DeleteTrainer(int trainerId)
-        //{
-        //    if (_TrainerRepository.Delete(trainerId))
-        //    {
-        //        return NoContent();
-        //    }
-        //    else
-        //    {
-        //        return NotFound();
-        //    }
-        //}
-
-        //[HttpPut("{trainerId:int}")]
-        //[ProducesResponseType(501)]
-        //public IActionResult UpdateTrainer(int trainerId, TrainerDTO trainer)
-        //{
-        //    return StatusCode(StatusCodes.Status501NotImplemented, new { reason = "Flemme" });
-        //}
+        [HttpPut("{trainerId:int}")]
+        [ProducesResponseType(501)]
+        public IActionResult UpdateCourse(int courseId, CourseDTO course)
+        {
+            Course courseToUpdate = new Course
+            {
+                Name = course.Name,
+                Description = course.Description
+            };
+            if (_CourseRepository.Update
+            return StatusCode(StatusCodes.Status501NotImplemented, new { reason = "Flemme" });
+        }
     }
 }
